7498ef3f321aa97d0fa1652edc4e0673ea53827b
Belen De Franchi <belendefranchi@gmail.com>
2022-08-25 22:00:39 -0300

    Desafío nro. 1

--- clase_5.js
+++
@@ -1,8 +0,0 @@
-/*############ OBJETOS LITERALES ############*/
-
-const operador1 = {
-    nombre: "Juan",
-    apellido: "Moon",
-    cargo: "Servicio técnico",
-    fechaIngreso: "2005-08-30",
-}
--- desafios.js
+++ desafios.js
@@ -1,9 +1,38 @@
 /* #############  DESAFIO NRO. 1  ############# */
 
+function calculadora(){
+    
+    let num1=parseFloat(prompt("Ingresa el primer nro:"))
+    let num2=parseFloat(prompt("Ingresa el segundo nro:"))
+    let ope=prompt("Ingresa el operador:")
+
+    console.log("Resultado: ", calcular(num1, num2, ope))
+}
+
+function calcular(num1, num2, ope){
+    switch(ope){
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            if (num2 === 0){
+                do{
+                    num2 = prompt("No se puede dividir por cero, ingresa otro número")
+                }while(num2 == 0)
+                return num1 / num2
+            }
+            return num1 / num2
+        default:
+            return "Error en el cálculo"
+        }
+    }
 
 
 
-/* #############  DESAFIO NRO. 2  ############# */
+
 
 
 
